ff2d66fcbd863e606679cf1533b2e753d4daadc5
CharlesAMoss <charlesamoss@yahoo.com>
2013-05-20 12:25:17 -0700

    added nomalize
    begun building of scss file
    import added

--- config.rb
+++ config.rb
@@ -6,12 +6,14 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+fonts_dir = "css/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
